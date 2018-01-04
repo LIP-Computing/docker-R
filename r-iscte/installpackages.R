063c9f025ec9a024426a357213eaedd0a98ff4b6
mariojmdavid <mariojmdavid@gmail.com>
2018-01-04 16:33:21 +0000

    new dockerfile

---
+++ r-iscte/installpackages.R
@@ -0,0 +1,16 @@
+#specify the packages of interest
+packages = c("multiplex", "network", "readxl", "sna", "ergm")
+
+#use this function to check if each package is on the local machine
+#if a package is installed, it will be loaded
+#if any are not, the missing package(s) will be installed and loaded
+package.check <- lapply(packages, FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+        install.packages(x, dependencies = TRUE)
+        library(x, character.only = TRUE)
+    }
+})
+
+#verify they are loaded
+search()
+
